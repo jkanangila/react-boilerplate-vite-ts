7235c3a67c9ff3948434afbb9829dc23b4741e5c
jkanangila <jkanangila@gmail.com>
2023-02-27 14:50:11 +0100

    install/configure tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <h1 className="text-3xl font-bold underline">Hello World</h1>
   </React.StrictMode>,
 );
